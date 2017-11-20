6765d4285e4bb4afb5e673e4cc9a78a6d6f5b0e0
StivenTC <stivent367@gmail.com>
2017-11-20 02:21:58 -0500

    Add topic page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { NewsPage } from '../pages/news/news';
 import { LoginPage } from '../pages/login/login';
 import { LessonsPage } from '../pages/lessons/lessons';
+import { TopicPage } from '../pages/topic/topic';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import { TICSOR } from './app.component';
     HomePage,
     NewsPage,
     LoginPage,
-    LessonsPage
+    LessonsPage,
+    TopicPage
   ],
   imports: [
     BrowserModule,
@@ -33,7 +35,8 @@ import { TICSOR } from './app.component';
     HomePage,
     NewsPage,
     LoginPage,
-    LessonsPage
+    LessonsPage,
+    TopicPage
   ],
   providers: [
     GooglePlus,
