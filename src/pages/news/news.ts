91971afd5f258acba726e486abb07a72ed7df4db
Zeroga98 <sebastianrojasgarzon@hotmail.com>
2017-11-26 04:28:52 -0500

    style

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ActionSheetController } from 'ionic-angular';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer';
 
 @Component({
   selector: 'page-news',
   templateUrl: 'news.html'
 })
+
 export class NewsPage {
+  
 
   constructor(
     public navCtrl: NavController,
-    public actionSheetCtrl: ActionSheetController) {
-
+    public actionSheetCtrl: ActionSheetController,
+    private document: DocumentViewer) {
+      const options: DocumentViewerOptions = {
+        title: 'My PDF'
+      }
+      this.document.viewDocument('assets/pdf-sample.pdf', 'application/pdf', options);
   }
+  
   presentActionSheet() {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'TicSor',
